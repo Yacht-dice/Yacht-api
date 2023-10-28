b17efcf6b201ff833f9dc1dc023622cbb563d518
yerin03 <kyr2532@soongsil.ac.kr>
2023-10-28 14:10:55 +0900

    first commit

---
+++ src/main/java/com/example/yatchdice/YatchDiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.yatchdice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YatchDiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YatchDiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yatchdice/YatchDiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yatchdice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YatchDiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
