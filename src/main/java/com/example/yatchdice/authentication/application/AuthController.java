2796da0b24a15cf3dd17c9c34e851a693095f36f
nohy6630 <nohy6630@gmail.com>
2023-11-28 15:38:01 +0900

    feat: swagger적용

--- src/main/java/com/example/yatchdice/authentication/application/AuthController.java
+++ src/main/java/com/example/yatchdice/authentication/application/AuthController.java
@@ -3,6 +3,8 @@ package com.example.yatchdice.authentication.application;
 import com.example.yatchdice.authentication.domain.AuthTokens;
 import com.example.yatchdice.authentication.infra.kakao.KakaoLoginParams;
 import com.example.yatchdice.authentication.infra.naver.NaverLoginParams;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "auth", description = "로그인 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
@@ -17,12 +20,26 @@ public class AuthController {
     private final OAuthLoginService oAuthLoginService;
 
     // POST 요청 "/api/auth/kakao" 매핑
+    @Operation(summary = "카카오 인가코드 이용하여 토큰 발급받기", description = """
+            카카오 인가코드를 request body에 담아 전달해주세요.
+            
+            response 값에 들어있는 acccessToken을 저장했다가 앞으로 서버에 api요청시 사용해주세요.
+            
+            후에 accessToken 사용시 request header에 Authorization값에 "Bearer 전달받은엑세스토큰" 형식으로 사용해주세요.
+            """)
     @PostMapping("/kakao")
     public ResponseEntity<AuthTokens> loginKakao(@RequestBody KakaoLoginParams params) { // Athorization Code를 파라미터로 받음
         // oAuthLoginService의 login 메서드를 호출, 인증 토큰을 받아서 반환
         return ResponseEntity.ok(oAuthLoginService.login(params));
     }
 
+    @Operation(summary = "네이버 인가코드 이용하여 토큰 발급받기", description = """
+            네이버 인가코드를 request body에 담아 전달해주세요.
+            
+            response 값에 들어있는 acccessToken을 저장했다가 앞으로 서버에 api요청시 사용해주세요.
+            
+            후에 accessToken 사용시 request header에 Authorization값에 "Bearer 전달받은엑세스토큰" 형식으로 사용해주세요.
+            """)
     @PostMapping("/naver")
     public ResponseEntity<AuthTokens> loginNaver(@RequestBody NaverLoginParams params) {
         return ResponseEntity.ok(oAuthLoginService.login(params));
