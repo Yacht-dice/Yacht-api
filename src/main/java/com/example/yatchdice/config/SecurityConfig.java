5f4ea4bebb5003ed4e49b1475b3d154b1cc645a8
Yeongjin Noh <129354455+nohy6630@users.noreply.github.com>
2023-11-28 15:44:14 +0900

    feat: swagger적용 (#13)

--- src/main/java/com/example/yatchdice/config/SecurityConfig.java
+++ src/main/java/com/example/yatchdice/config/SecurityConfig.java
@@ -38,6 +38,9 @@ public class SecurityConfig {
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.ignoring()
                 .requestMatchers(
+                        "/swagger-resources/**",
+                        "/v3/api-docs/**",
+                        "/swagger-ui/**",
                         "/api/auth/**",
                         "/deploy/**"); //일단 시큐리티 모든 경로에 대해서 무시하게 설정, 나중에 로그인 구현한뒤 풀어줘야 함
     }
