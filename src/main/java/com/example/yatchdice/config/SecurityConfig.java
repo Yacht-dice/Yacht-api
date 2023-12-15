f97f5401c97e0b700fd63bf2289723005378c3ad
nohy6630 <nohy6630@gmail.com>
2023-12-15 22:23:08 +0900

    fix: cors 설정 수정

--- src/main/java/com/example/yatchdice/config/SecurityConfig.java
+++ src/main/java/com/example/yatchdice/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
 
     private CorsConfiguration getDefaultCorsConfiguration() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));// 모든 ip 응답을 허용
+        configuration.setAllowedOrigins(List.of("https://yacht-dice-ten.vercel.app"));// 모든 ip 응답을 허용
         configuration.setAllowedHeaders(List.of("*")); // 모든 header 에 응답을 허용
         configuration.setAllowedMethods(List.of("*")); // 모든 get,post,patch,put,delete 요청 허용
         configuration.setAllowCredentials(true); // 내 서버가 응답할 때 json 을 자바스크립트에서 처리할 수 있게 할지를 설정하는 것
